29179cf640035864e23ab86eb7cd5ffb52c5aff3
Askar Nurbekov <nurbekoff@gmail.com>
2023-11-04 18:37:04 -0500

    Delete file_wait.py

--- file_wait.py
+++
@@ -1,6 +0,0 @@
-# file_wait.py
-with open('/tmp/testfile.txt', 'w') as f:
-    print("File is open. Please enter any text and press enter:")
-    user_input = input()
-    f.write(user_input)
-    print("You've written to the file!")
